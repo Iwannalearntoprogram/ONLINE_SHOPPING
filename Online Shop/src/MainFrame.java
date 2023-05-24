0d261577194c6a1607c81bffac39683c8fb4019a
Carl Jay Margate <76047427+Iwannalearntoprogram@users.noreply.github.com>
2023-05-24 22:09:12 +0800

    Update with functioning bg

--- Online Shop/src/MainFrame.java
+++ Online Shop/src/MainFrame.java
@@ -15,79 +15,34 @@ import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame{
 
-    public ImageIcon bg;
-    public JLabel bglabel;
-    public JFrame admin;
-
-
     MainFrame(){
 
 
-        bg = new ImageIcon("Sakura.gif");
-        bglabel = new JLabel(bg);
-        bglabel.setSize(1300, 900);
+        //Image icon
+        ImageIcon bg= new ImageIcon("giphy.gif");
+        //Adds background label 
+        JLabel bglabel = new JLabel();
+
+        bglabel.setIcon(bg);
 
 
-        admin = new JFrame();
-        admin.add(bglabel);
-        admin.setLayout(new FlowLayout());
-        admin.setTitle("NAOIMISE");
-        admin.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        admin.setSize(1300, 900);
-        admin.setLocationRelativeTo(null);
-        admin.setVisible(true);
-        admin.setResizable(false);
-        admin.setContentPane( new MainFrame("Sakura.gif"));
-       
+        this.add(bglabel);
+        this.setLayout(new FlowLayout());
+        this.setTitle("NAOIMISE");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(1300, 900);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.getContentPane().setBackground(Color.decode("#F8E8EE"));
 
 
-    }public void paintComponent(Graphics g){
+        
 
 
-
-
+    
     }
 
-
-
-
-
-
-
-
-
-    
- }
-
-
         
-    
-
-
-     
+            }
         
-
-
-
-
-
-
-
-       
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-    
-    
-
