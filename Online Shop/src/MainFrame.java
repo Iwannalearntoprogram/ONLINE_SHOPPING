e59abd5a4982f25070804b2426a77827a40c08aa
K <120694178+KENJIMENDOZA@users.noreply.github.com>
2023-06-18 06:52:48 -1200

    Update MainFrame.java

--- Online Shop/src/MainFrame.java
+++ Online Shop/src/MainFrame.java
@@ -3,16 +3,22 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainFrame extends JFrame {
     private JPanel productPanel;
-    private JButton deliverButton;
+    protected JButton deliverButton;
     private JButton searchButton;
-    private JButton cartButton;
+    protected JButton cartButton;
     private JLabel breadLabel;
     private JButton breadMinusButton;
     private JButton breadPlusButton;
@@ -23,9 +29,11 @@ public class MainFrame extends JFrame {
     private JButton ramenMinusButton;
     private JButton ramenPlusButton;
 
+    private Map<String, ImageIcon> productImages;
+
     public MainFrame() {
         setTitle("NAOIMISE");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(1100, 943);
         setResizable(false);
         setLayout(null);
@@ -53,6 +61,7 @@ public class MainFrame extends JFrame {
         // Create and customize the buttons
         deliverButton = new JButton("Deliver");
         deliverButton.setBounds(20, 100, 200, 50);
+        deliverButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
         backgroundLabel.add(deliverButton);
 
         searchButton = new JButton("SEARCH");
@@ -62,21 +71,28 @@ public class MainFrame extends JFrame {
 
         cartButton = new JButton("Cart");
         cartButton.setBounds(getWidth() - 220, 160, 200, 50);
+        cartButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
         backgroundLabel.add(cartButton);
 
         breadLabel = new JLabel(new ImageIcon("Bread.png"));
         breadMinusButton = new JButton("-");
         breadPlusButton = new JButton("+");
+        breadMinusButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
+        breadPlusButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
         customizeProductButton(breadLabel, breadMinusButton, breadPlusButton);
 
         chocolateLabel = new JLabel(new ImageIcon("Chocolate.png"));
         chocolateMinusButton = new JButton("-");
         chocolatePlusButton = new JButton("+");
+        chocolateMinusButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
+        chocolatePlusButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
         customizeProductButton(chocolateLabel, chocolateMinusButton, chocolatePlusButton);
 
         ramenLabel = new JLabel(new ImageIcon("Ramen.png"));
         ramenMinusButton = new JButton("-");
         ramenPlusButton = new JButton("+");
+        ramenMinusButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
+        ramenPlusButton.setFont(new Font("joystix monospace", Font.BOLD, 20));
         customizeProductButton(ramenLabel, ramenMinusButton, ramenPlusButton);
 
         // Adjust dimensions and positions for the product elements
@@ -121,22 +137,80 @@ public class MainFrame extends JFrame {
         productPanel.add(chocolatePanel);
         productPanel.add(ramenPanel);
 
+        // Initialize product images
+        productImages = new HashMap<>();
+        productImages.put("Bread", new ImageIcon("Bread.png"));
+        productImages.put("Chocolate", new ImageIcon("Chocolate.png"));
+        productImages.put("Ramen", new ImageIcon("Ramen.png"));
+
+        // Add action listener to the search button
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showSearchWindow();
+            }
+        });
+
         setVisible(true);
     }
 
     private void customizeProductButton(JLabel productLabel, JButton minusButton, JButton plusButton) {
-    productLabel.setFont(new Font("joystix monospace", Font.BOLD, 40));
-    minusButton.setFont(new Font("joystix monospace", Font.BOLD, 30)); // Adjusted font size
-    plusButton.setFont(new Font("joystix monospace", Font.BOLD, 30)); // Adjusted font size
-    Color buttonColor = new Color(255, 255, 255);
-    minusButton.setBackground(buttonColor);
-    plusButton.setBackground(buttonColor);
-    minusButton.setPreferredSize(new Dimension(60, 50)); // Fixed button size
-    plusButton.setPreferredSize(new Dimension(60, 50)); // Fixed button size
-}
-
-    public static void main(String[] args) {
-        new MainFrame();
+        productLabel.setFont(new Font("joystix monospace", Font.BOLD, 40));
+        productLabel.setForeground(Color.WHITE); // Set label text color
+        minusButton.setFont(new Font("joystix monospace", Font.BOLD, 30));
+        plusButton.setFont(new Font("joystix monospace", Font.BOLD, 30));
+        Color buttonColor = new Color(255, 255, 255);
+        minusButton.setBackground(buttonColor);
+        plusButton.setBackground(buttonColor);
+        minusButton.setPreferredSize(new Dimension(60, 50));
+        plusButton.setPreferredSize(new Dimension(60, 50));
     }
-}
 
+    private void showSearchWindow() {
+        JFrame searchFrame = new JFrame("Search Products");
+        searchFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        searchFrame.setFont(new Font("joystix monospace", Font.BOLD, 30));
+        searchFrame.setSize(400, 300);
+        searchFrame.setLayout(new FlowLayout());
+        
+        // Create search label and text field
+        JLabel searchLabel = new JLabel("Search: ");
+        JTextField searchField = new JTextField(20);
+        searchLabel.setFont(new Font("joystix monospace", Font.BOLD, 10));
+        
+        // Create search button
+        JButton searchButton = new JButton("Search");
+        searchButton.setFont(new Font("joystix monospace", Font.BOLD, 10));
+        
+        // Add action listener to the search button
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String searchText = searchField.getText();
+                if (productImages.containsKey(searchText)) {
+                    // If the searched product exists, create and show a new window with the product image
+                    JFrame resultFrame = new JFrame("Search Result");
+                    resultFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                    resultFrame.setSize(400, 400);
+                    resultFrame.setLayout(new FlowLayout());
+                    
+                    JLabel resultLabel = new JLabel(productImages.get(searchText));
+                    resultFrame.add(resultLabel);
+                    
+                    resultFrame.setVisible(true);
+                }
+            }
+        });
+        
+        // Add components to the search frame
+        searchFrame.add(searchLabel);
+        searchFrame.add(searchField);
+        searchFrame.add(searchButton);
+        
+        searchFrame.setVisible(true);
+    }
+
+    // public static void main(String[] args) {
+    //     new MainFrame();
+    // }
+}
