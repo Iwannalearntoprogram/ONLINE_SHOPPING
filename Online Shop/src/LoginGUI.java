86885ea128f86690a194de0960967f80782ace68
K <120694178+KENJIMENDOZA@users.noreply.github.com>
2023-06-26 07:52:17 -1200

    Update LoginGUI.java

--- Online Shop/src/LoginGUI.java
+++ Online Shop/src/LoginGUI.java
@@ -1,163 +1,179 @@
+package src;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.*;
 
+import src.Customs.CustomButton;
+import src.Customs.CustomPasswordField;
+import src.Customs.CustomTextField;
+
 public class LoginGUI implements ActionListener {
 
-    private JPanel contentPane;
-    private JPanel mainPane;
-    private JButton logButton;
-    private JButton regButton;
-    private JLabel titleLabel;
+	private JPanel contentPane;
+	private JPanel mainPane;
+    private CustomButton logButton;
+	private CustomButton regButton;
+	private JLabel titleLabel;
     private CustomTextField userText;
     private CustomPasswordField passwordText;
     private JFrame frame;
+      
+	private java.net.URL dataData;
 
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new LoginGUI();
             }
         });
-    }
+	}
 
-    public LoginGUI() {
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1100, 943);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setTitle("NAOIMISE");
+	
+	public LoginGUI() {
+        dataData = getClass().getResource("data/data.txt");
 
-        contentPane = new JPanel(new BorderLayout()) {
-            @Override
-            public void paintComponent(Graphics g) {
-                try {
-                    g.drawImage(ImageIO.read(new File("Background.png")), 0, 0, null);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-        mainPane = new JPanel();
-        mainPane.setOpaque(false);
-        mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.PAGE_AXIS));
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1100, 943);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setTitle("NAOIMISE");
 
-        Dimension buttonSize = new Dimension(300, 50);
+		URL back = getClass().getResource("resources/Background.png");
+		
+		contentPane = new JPanel(new BorderLayout()) {
+	        @Override public void paintComponent(Graphics g) {
+	            try {
+					g.drawImage( (new ImageIcon(back).getImage()), 0, 0, null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+	        }
+		};
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		mainPane = new JPanel();	
+		mainPane.setOpaque(false);
+		mainPane.setLayout(new BoxLayout(mainPane, BoxLayout.PAGE_AXIS));
+		
 
-        titleLabel = new JLabel("LOGIN");
-        titleLabel.setOpaque(false);
-        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        titleLabel.setFont(new Font("joystix monospace", Font.PLAIN, 50));
+        Dimension buttonSize = new Dimension(300,50);
+		
+		titleLabel = new JLabel("LOGIN");
+		titleLabel.setOpaque(false);
+		titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		titleLabel.setFont(new Font("joystix monospace", Font.PLAIN, 50));
+		
+		
+		userText = new CustomTextField();
+		userText.setAlignmentX(Component.CENTER_ALIGNMENT);
+		userText.setLabelText("USERNAME");
+		userText.setFont(new Font("joystix monospace", Font.PLAIN, 18));
+		userText.setPreferredSize( new Dimension(300,60));
+		userText.setMinimumSize(userText.getPreferredSize());
+		userText.setMaximumSize(userText.getPreferredSize());
 
-        userText = new CustomTextField();
-        userText.setAlignmentX(Component.CENTER_ALIGNMENT);
-        userText.setLabelText("USERNAME");
-        userText.setFont(new Font("joystix monospace", Font.PLAIN, 18));
-        userText.setPreferredSize(new Dimension(300, 60));
-        userText.setMinimumSize(userText.getPreferredSize());
-        userText.setMaximumSize(userText.getPreferredSize());
+	
+		passwordText = new CustomPasswordField();
+		passwordText.setAlignmentX(Component.CENTER_ALIGNMENT);
+		passwordText.setLabelText("PASSWORD");
+		passwordText.setFont(new Font("joystix monospace", Font.PLAIN, 18));
+		passwordText.setPreferredSize( new Dimension(300,60));
+		passwordText.setMinimumSize(userText.getPreferredSize());
+		passwordText.setMaximumSize(userText.getPreferredSize());
 
-        passwordText = new CustomPasswordField();
-        passwordText.setAlignmentX(Component.CENTER_ALIGNMENT);
-        passwordText.setLabelText("PASSWORD");
-        passwordText.setFont(new Font("joystix monospace", Font.PLAIN, 18));
-        passwordText.setPreferredSize(new Dimension(300, 60));
-        passwordText.setMinimumSize(userText.getPreferredSize());
-        passwordText.setMaximumSize(userText.getPreferredSize());
+		Box box = new Box(BoxLayout.Y_AXIS);
+		box.add(Box.createVerticalGlue());
+		box.add(mainPane);
+		box.add(Box.createVerticalGlue());
+		
 
-        Box box = new Box(BoxLayout.Y_AXIS);
-        box.add(Box.createVerticalGlue());
-        box.add(mainPane);
-        box.add(Box.createVerticalGlue());
-
-        logButton = new CustomButton();
+        logButton = new CustomButton("Login");
         logButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        logButton.setText("Login");
-        logButton.setFont(new Font("joystix monospace", Font.PLAIN, 18));
         logButton.setPreferredSize(buttonSize);
         logButton.setMinimumSize(buttonSize);
         logButton.setMaximumSize(buttonSize);
-        logButton.addActionListener(this);
+        logButton.addActionListener(this); 
+		
+		
+		regButton = new CustomButton("Register");
+		regButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		regButton.setPreferredSize(buttonSize);
+		regButton.setMinimumSize(buttonSize);
+		regButton.setMaximumSize(buttonSize);
+	    regButton.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent ae) {
+	            // Open registration window
+	            RegGUI register = new RegGUI();
+	            register.setVisible(true);
+	        }
+	    });
+	    
+	    JPanel btnPanel = new JPanel(new FlowLayout());
+	    btnPanel.setOpaque(false);
+	    btnPanel.add(logButton);
+	    btnPanel.add(regButton);
+	    
+	   
 
-        regButton = new CustomButton();
-        regButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        regButton.setText("Register");
-        regButton.setFont(new Font("joystix monospace", Font.PLAIN, 18));
-        regButton.setPreferredSize(buttonSize);
-        regButton.setMinimumSize(buttonSize);
-        regButton.setMaximumSize(buttonSize);
-        regButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                // Open registration window
-                RegGUI register = new RegGUI();
-                register.setVisible(true);
+		mainPane.add(titleLabel);
+		mainPane.add(Box.createRigidArea(new Dimension(0,100)));
+		mainPane.add(userText);
+		mainPane.add(Box.createRigidArea(new Dimension(0,10)));
+		mainPane.add(passwordText);
+		mainPane.add(Box.createRigidArea(new Dimension(0,100)));
+		mainPane.add(btnPanel);
+		
+		contentPane.add(box, BorderLayout.CENTER);
+
+		frame.setContentPane(contentPane);
+		frame.setVisible(true);
+	}
+	
+	
+	@Override
+public void actionPerformed(ActionEvent ae) {
+    boolean matched = false;
+    String user = userText.getText().toString();
+    String password = new String(passwordText.getPassword());
+
+    try {
+        FileReader fr = new FileReader(new File(dataData.toURI()));
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.equals(user + "\t" + password)) {
+                matched = true;
+                break;
             }
-        });
-
-        JPanel btnPanel = new JPanel(new FlowLayout());
-        btnPanel.setOpaque(false);
-        btnPanel.add(logButton);
-        btnPanel.add(regButton);
-
-        mainPane.add(titleLabel);
-        mainPane.add(Box.createRigidArea(new Dimension(0, 100)));
-        mainPane.add(userText);
-        mainPane.add(Box.createRigidArea(new Dimension(0, 10)));
-        mainPane.add(passwordText);
-        mainPane.add(Box.createRigidArea(new Dimension(0, 100)));
-        mainPane.add(btnPanel);
-
-        contentPane.add(box, BorderLayout.CENTER);
-
-        frame.setContentPane(contentPane);
-        frame.setVisible(true);
+        }
+        fr.close();
+    } catch (Exception e) {
+        e.printStackTrace();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        boolean matched = false;
-        String user = userText.getText().toString();
-        String password = new String(passwordText.getPassword());
+    if (matched) {
+        if (user.toLowerCase().contains("admin")) {
+            JOptionPane.showMessageDialog(frame, "Welcome Admin!");
 
-        try {
-            FileReader fr = new FileReader("data.txt");
-            BufferedReader br = new BufferedReader(fr);
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.equals(user + "\t" + password)) {
-                    matched = true;
-                    break;
-                }
-            }
-            fr.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (matched) {
-            if (user.toLowerCase().contains("admin")) {
-                // Open admin window
-                new AdminFrame();
-            } else {
-                JOptionPane.showMessageDialog(frame, "Welcome!");
-                frame.dispose();
-                MainFrame mainFrame = new MainFrame(password);
-                mainFrame.setVisible(true);
-            }
+            frame.dispose();
+            AdminFrame adminFrame = new AdminFrame();
+            adminFrame.setVisible(true);
         } else {
-            JOptionPane.showMessageDialog(frame, "Invalid Username or Password");
+            JOptionPane.showMessageDialog(frame, "Welcome!");
+
+            frame.dispose();
+            MainFrame mainFrame = new MainFrame(userText.getText());
+            mainFrame.setVisible(true);
         }
+    } else {
+        JOptionPane.showMessageDialog(frame, "Invalid Username or Password");
     }
+}
 }
